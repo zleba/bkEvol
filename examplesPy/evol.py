8547b865ca00295c4a2d1104a3c55998e0285e3e
Radek Zlebcik <radek.zlebcik@cern.ch>
2018-11-18 23:15:26 +0100

    update of evolution notebook

--- examplesPy/evol.py
+++ examplesPy/evol.py
@@ -1,95 +1,82 @@
 #!/usr/bin/env python
-#%matplotlib inline
 import bkevol as bk
+
+# Load Setting object with configuration of the evolution
 
 S = bk.Settings()
 
-#Evolution settings
+# Value of the $\alpha_S(M_Z)$ and the $\alpha_S$ freezing scale
+
 S.alphaS = 0.118
 S.freezingScale = 1.0
+
+# The regularization variable eps and the scale $\mu$
+
 S.eps  = 1e-7
 S.mu2  = 1e-2
 
+# Number of the grid points in $k_T^2$ variable and the range in $k_T^2$
 
-S.NkT2 = 17
-S.NkT2int = 17
-
-#S.NkT2 = 257
-#S.NkT2int = 257
-
-
+S.NkT2 = S.NkT2int = 17
 S.kT2Min = 1e-2
 S.kT2Max = 1e6
 
+# Number of the grid points in $x$ variable and the xMin and xMax values
 
 S.Nrap = 64
 #S.Nrap = 512
 S.xMin = 1e-6
 S.xMax = 1
 
-
-#S.kernelType = "BFKLplain:sub" #)
-#S.kernelType = "BFKL:eps" #+sub)
-S.kernelType = "BFKL_res:Eps" #+sub)
-#S.kernelType = "BFKL_res_kc_simp:eps" #+sub)
-#S.kernelType = "BFKL_res_kc_v_r_simp:eps" #+sub)
-#S.kernelType = "BFKL_res_kc_full:eps" #+sub)
-#S.kernelType = "BFKL_res_kc_v_r_full:eps" #+sub)
-#S.kernelType = "BFKL_res_DGLAP:eps"
-#S.kernelType = "BFKL_res_kc_full_DGLAP:eps"
+# Function to be evolved (TODO)
 
 S.funStr = "function"
 S.pars = [(1,2, 3),
           (2,4., 5)]
 
-
-#S.printInfo()
-#import sys
-#sys.exit(0)
+# Overview of the implemented kernels
 
 kernels = [
- "BFKLplain:sub",
- "BFKL:Eps", "BFKL:sub",
- "BFKL_res:Eps", "BFKL_res:sub",
- "BFKL_res_kc_simp:Eps", "BFKL_res_kc_simp:sub",
- "BFKL_res_kc_v_r_simp:Eps", "BFKL_res_kc_v_r_simp:sub",
- "BFKL_res_kc_full:Eps", "BFKL_res_kc_full:sub",
- "BFKL_res_kc_v_r_full:Eps", "BFKL_res_kc_v_r_full:sub",
+ "BFKLplain:Sub",
+ "BFKL:Eps", "BFKL:Sub",
+ "BFKL_res:Eps", "BFKL_res:Sub",
+ "BFKL_res_kc_simp:Eps", "BFKL_res_kc_simp:Sub",
+ "BFKL_res_kc_v_r_simp:Eps", "BFKL_res_kc_v_r_simp:Sub",
+ "BFKL_res_kc_full:Eps", "BFKL_res_kc_full:Sub",
+ "BFKL_res_kc_v_r_full:Eps", "BFKL_res_kc_v_r_full:Sub",
  "BFKL_res_DGLAP:Eps",
  "BFKL_res_kc_full_DGLAP:Eps",
  "BFKL_res_kc_full_DGLAP_simp_kc:Eps",
  "BFKL_res_kc_full_DGLAP_full_kc:Eps",
 ]
 
+# Evolution kernel to be used
 
-S.kernelType = "BFKL:Sub" #+sub)
-#S.kernelType = "BFKLplain:Sub"
-#S.kernelType = "BFKL_res_DGLAP:Eps"
-#S.kernelType = "BFKL_res_DGLAP:ZEps"
+S.kernelType = "BFKL:Sub"
 
-#S.kernelType = "BFKL_res_kc_full_DGLAP:Eps"
-#S.kernelType = "BFKL_res_kc_full_DGLAP_simp_kc:Eps"
-#S.kernelType = "BFKL_res_kc_full_DGLAP_simp_kc:ZEps"
+# Calculate the evolution kernel
 
-#S.kernelType = "BFKL_res_kc_full_DGLAP_full_kc:Eps"
-
-print S.alphaS
-
-#for k in kernels:
-#S.kernelType = k
 sol = bk.Solver(S)
 sol.CalcEvolKernel()
 
-#sol.EvolveAll()
-#sol.PrintBaseGrid();
+# Posibility to save the evolution kernel
 
-sol.SaveEvolKernels("testNow.h5")
-#sol.LoadEvolKernels("testNow.h5")
+sol.SaveEvolKernels("evolKernel.h5")
+#sol.LoadEvolKernels("evolKernel.h5")
+
+# Evolve with the function in the Settings
+
 sol.EvolveAll()
+
+# Get the result of the evolution and the grid points
+
 #sol.PrintBaseGrid()
 res = sol.getResult()
 k2Grid = sol.getK2grid()
 
+# Plot results
+
+#%matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
 for i,r in enumerate(res[::6]):
